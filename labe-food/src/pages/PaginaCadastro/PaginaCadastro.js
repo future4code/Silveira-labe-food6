ac6fce8ea31cc103036dd4c4c884170b47540596
Diego Eduardo Lima <deduardolima>
2022-05-16 12:24:47 -0300

    arquivo inicial

---
+++ labe-food/src/pages/PaginaCadastro/PaginaCadastro.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const PaginaCadastro = () => {
+  return (
+    <div>PaginaCadastro</div>
+  )
+}
+export default PaginaCadastro;
---
+++ labe-food/src/routes/coordinator.js
@@ -0,0 +1,18 @@
+export const irParaLogin = (navigate) => {
+    navigate("/")
+}
+export const irParaCadastro = (navigate) => {
+    navigate("/cadastro")
+}
+export const irParaHome = (navigate) => {
+    navigate("/home/")
+}
+export const irParaPerfil = (navigate) => {
+    navigate("/perfil")
+}
+export const irParaCarrinho = (navigate) => {
+    navigate("/carrinho")
+}
+export const goBack = (navigate) => {
+    navigate(-1)
+}
