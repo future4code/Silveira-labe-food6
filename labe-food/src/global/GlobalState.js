3d1cdc572566d91c9ea5b6337d116f45f7559962
Diego Eduardo Lima <deduardolima>
2022-05-17 19:51:29 -0300

    estado global

---
+++ labe-food/src/global/GlobalState.js
@@ -0,0 +1,42 @@
+import { GlobalStateContext } from "./GlobalStateContext";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../constants/urls";
+
+
+export default function GlobalState(props) {
+    const [restaurantList, setRestaurantList] = useState([]);
+
+    const getRestaurantList = () => {//pegar a lista de restaurantes da API
+        axios.get(`${BASE_URL}/restaurants`,{
+            headers: {
+                Auth: localStorage.getItem('token')
+            }
+        })
+            .then((response) => {
+                console.log(response)
+                // setRestaurantList(response.data.results);
+            })
+            .catch((error) => console.log(error.message));
+    };
+
+
+    useEffect(() => {
+        getRestaurantList();
+    }, []);
+
+
+    const data = { 
+        restaurantList,
+        setRestaurantList,      
+    };
+
+    return (
+        <div>
+            <GlobalStateContext.Provider value={data} >
+                {props.children}
+            </GlobalStateContext.Provider>
+        </div>
+    )
+
+} 
--- labe-food/src/pages/PaginaHome/PaginaHome.js
+++ labe-food/src/pages/PaginaHome/PaginaHome.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+// import { GlobalStateContext } from "../../global/GlobalStateContext";
+// import { useNavigate } from "react-router-dom";
 
 const PaginaHome = () => {
+  // const { restaurantList } = useContext(GlobalStateContext);
+  // const navigate = useNavigate()
+  //   console.log(restaurantList)
   return (
     <div>PaginaHome</div>
   )
